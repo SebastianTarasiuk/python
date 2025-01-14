04f5e8533b4d1426b564d74a859467d0aab1adff
Twoje Imię i Nazwisko <alicja.mega@gmail.com>
2025-01-14 09:21:06 +0100

    Klasa miejscowosc + Miasto

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ class Miejscowosc:
         self.liczba = liczba
         self.powierzchnia = powierzchnia
 
+class Miasto(Miejscowosc):
+    def __init__(self, nazwa, liczba, powierzchnia, prezydent, powiat):
+        super().__init__(nazwa, liczba, powierzchnia)
+        self.prezydent = prezydent
+        self.powiat = powiat
 
 
 
