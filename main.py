1f514572a6f47ea79e9dc4a9bfc4cd750c7ff022
Sebatian <sebastian.tarasiuk2005@gmail.com>
2025-01-14 09:40:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+"""
+Author: [Sebastian Tarasiuk]
+Date: [14,01,2024]
+Course: Python I, Lab 13
+Assignment: [własny przykład dziedziczenia]
+Description:
+    [Stowrzyłem 2 obiekty ]
+Version: [Wersja 1.0]
+Notes:
+    [Zadanie bardzo przyjemne, ponieważ miałem jasne polecenie i łatwo było mi znaleźć potrzebną mi wiedzę :D]
+"""
+
+
+
 class Miejscowosc:
     def __init__(self, nazwa, liczba, powierzchnia):
         self.nazwa = nazwa
