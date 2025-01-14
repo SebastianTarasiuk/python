5cb43b51eee5ca8d2bf60ff6caf3b458552fcb23
Twoje Imię i Nazwisko <alicja.mega@gmail.com>
2025-01-14 09:20:42 +0100

    Klasa miejscowosc

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Miejscowosc:
+    def __init__(self, nazwa, liczba, powierzchnia):
+        self.nazwa = nazwa
+        self.liczba = liczba
+        self.powierzchnia = powierzchnia
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Seba')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
